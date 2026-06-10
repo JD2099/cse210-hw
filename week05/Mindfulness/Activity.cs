b2f65f37a48a72e6866322cfa30ca3f7566ea2f1
JD2099 <juandiego2099@gmail.com>
2026-06-10 14:47:45 -0500

    Update W05

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+
+        Console.WriteLine($"Welcome to the {_name}");
+        Console.WriteLine();
+        Console.WriteLine(_description);
+        Console.WriteLine();
+
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Well done!");
+        ShowSpinner(3);
+
+        Console.WriteLine();
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.");
+        ShowSpinner(3);
+
+        Console.WriteLine();
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        List<string> spinner = new List<string> { "|", "/", "-", "\\" };
+
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(spinner[i]);
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= spinner.Count)
+            {
+                i = 0;
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    public void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+
+        Console.WriteLine();
+    }
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        int choice = 0;
+
+        while (choice != 4)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+
+            Console.Write("Select a choice: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    new BreathingActivity().Run();
+                    break;
+
+                case 2:
+                    new ReflectionActivity().Run();
+                    break;
+
+                case 3:
+                    new ListingActivity().Run();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
     }
 }
